75e6b9707346a7bff96ceed1f222b0642adbc39a
xmansx1 <your@email.com>
2025-08-25 14:33:47 +0300

    الرفع02ريندر

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------- الأمان -----------------
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret")
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
-ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",") if h.strip()]
+ALLOWED_HOSTS = ["school-7lgm.onrender.com", "localhost", "127.0.0.1"]
 
 # ----------------- التطبيقات -----------------
 INSTALLED_APPS = [
